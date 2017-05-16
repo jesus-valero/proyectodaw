b49ee429ef8f9598255d8b647211517bb62ae864
jesus-valero <bunyuelo@gmail.com>
2017-05-16 16:53:48 +0200

    [Map] Encode de array a Json desde controller

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -8,9 +8,20 @@ class Home extends CI_Controller {
 		$this->load->view('vHome');
 	}
 
-	public function map()	{
+	public function map() {
 
-		$this->load->view('vMap');
+		$locations = array(
+			array('lat' => 41.5053062, 'lng' => 2.1176510000000004, 'type' => 'bar', 'title' => 'Titulo Prueba 1', 'text' => 'Texto de prueba 1'),
+			array('lat' => 41.5153062, 'lng' => 2.1176510000000004, 'type' => 'sport', 'title' => 'Titulo Prueba 2', 'text' => 'Texto de prueba 2'),
+			array('lat' => 41.5153062, 'lng' => 2.1276510000000004, 'type' => 'arts', 'title' => 'Titulo Prueba 3', 'text' => 'Texto de prueba 3'),
+			array('lat' => 41.5053062, 'lng' => 2.1276510000000004, 'type' => 'bar', 'title' => 'Titulo Prueba 4', 'text' => 'Texto de prueba 4')
+			);
+
+		$locations = json_encode($locations);	
+
+		$data = array('locations' => $locations);
+
+		$this->load->view('vMap', $data);
 	}
 
 }
--- application/views/vMap.php
+++ application/views/vMap.php
@@ -23,110 +23,33 @@
 
     function myMap() {
 
-     var pos = {lat: -36.828611, lng: 2.1178017};
+      //array que contiene los datos que recibimos de la BBDD
+      var locations = <?php echo $locations; ?>
 
-     //array iconos para leyenda
-     var icons = {
-      bar: {
-        name: 'Bar',
-        icon: '../img/map/icons/bar.png'
-      },
-      arts: {
-        name: 'Arts',
-        icon: '../img/map/icons/arts.png'
-      },
-      sport: {
-        name: 'Sport',
-        icon: '../img/map/icons/sport.png'
-      }
-    };
+      var pos = {lat: -36.828611, lng: 2.1178017};
 
-    var mapProp = {
+      //array iconos para leyenda
+      var icons = {
+        bar: {
+          name: 'Bar',
+          icon: '../img/map/icons/bar.png'
+        },
+        arts: {
+          name: 'Arts',
+          icon: '../img/map/icons/arts.png'
+        },
+        sport: {
+          name: 'Sport',
+          icon: '../img/map/icons/sport.png'
+        }
+      };
+
+      var mapProp = {
         center: pos, //coordenadas decimales
         zoom:15, //valores de 1 a 23
         mapTypeId: google.maps.MapTypeId.ROADMAP, // valores ROADMAP, SATELLITE, HYBRID, TERRAIN
-        //estilos del mapa(descomentar y dar valores para modificar estilo)
-        styles: [
-        /*{elementType: 'geometry', stylers: [{color: '#242f3e'}]},*/
-        /* {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},*/
-        /*{elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},*/
-            /*{
-              featureType: 'administrative.locality',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#d59563'}]
-            },*/
-            /*{
-              featureType: 'poi',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#102EB3'}]
-            },*/
-           /* {
-              featureType: 'poi.park',
-              elementType: 'geometry',
-              stylers: [{color: '#263c3f'}]
-            },*/
-            /*{
-              featureType: 'poi.park',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#6b9a76'}]
-            },*/
-            /*{
-              featureType: 'road',
-              elementType: 'geometry',
-              stylers: [{color: '#38414e'}]
-            },*/
-            /*{
-              featureType: 'road',
-              elementType: 'geometry.stroke',
-              stylers: [{color: '#212a37'}]
-            },*/
-            /*{
-              featureType: 'road',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#9ca5b3'}]
-            },*/
-            /*{
-              featureType: 'road.highway',
-              elementType: 'geometry',
-              stylers: [{color: '#746855'}]
-            },*/
-            /*{
-              featureType: 'road.highway',
-              elementType: 'geometry.stroke',
-              stylers: [{color: '#1f2835'}]
-            },*/
-           /* {
-              featureType: 'road.highway',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#f3d19c'}]
-            },*/
-            /*{
-              featureType: 'transit',
-              elementType: 'geometry',
-              stylers: [{color: '#2f3948'}]
-            },*/
-            /*{
-              featureType: 'transit.station',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#d59563'}]
-            },*/
-            /*{
-              featureType: 'water',
-              elementType: 'geometry',
-              stylers: [{color: '#17263c'}]
-            },*/
-            /*{
-              featureType: 'water',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#515c6d'}]
-            },*/
-            /*{
-              featureType: 'water',
-              elementType: 'labels.text.stroke',
-              stylers: [{color: '#17263c'}]
-            }*/
-            ]
-          };      
+        //estilos del mapa(descomentar y dar valores para modificar estilo)       
+      };      
 
       var map=new google.maps.Map(document.getElementById("googleMap"),mapProp); //el mapa se pintará en id googleMap
 
@@ -183,34 +106,7 @@
       //indicamos la posicion donde queremos la leyenda
       map.controls[google.maps.ControlPosition.RIGHT_TOP].push(legend);
 
-    }
-   
-    //array que contiene los datos que mas adelante recibiremos de la BBDD
-    var locations = [
-    {lat: 41.5053062, lng: 2.1176510000000004, type: 'bar', title: 'Titulo Prueba 1', text: 'Texto de prueba 1'},
-    {lat: 41.5153062, lng: 2.1176510000000004, type: 'sport', title: 'Titulo Prueba 2', text: 'Texto de prueba 2'},
-    {lat: 41.5153062, lng: 2.1276510000000004, type: 'arts', title: 'Titulo Prueba 3', text: 'Texto de prueba 3'},
-    {lat: 41.5053062, lng: 2.1276510000000004, type: 'bar', title: 'Titulo Prueba 4', text: 'Texto de prueba 4'},
-    {lat: -33.851702, lng: 151.216968, type: 'bar', title: 'Titulo Prueba 1', text: 'Texto de prueba 1'},
-    {lat: -34.671264, lng: 150.863657, type: 'sport', title: 'Titulo Prueba 1', text: 'Texto de prueba 1'},
-    {lat: -35.304724, lng: 148.662905, type: 'arts', title: 'Titulo Prueba 1', text: 'Texto de prueba 1'},
-    {lat: -36.817685, lng: 175.699196, type: 'bar', title: 'Titulo Prueba 1', text: 'Texto de prueba 1'},
-    {lat: -36.828611, lng: 175.790222, type: 'sport', title: 'Titulo Prueba 1', text: 'Texto de prueba 1'},
-    {lat: -37.750000, lng: 145.116667, type: 'arts', title: 'Titulo Prueba 1', text: 'Texto de prueba 1'},
-    {lat: -37.759859, lng: 145.128708, type: 'bar', title: 'Titulo Prueba 1', text: 'Texto de prueba 1'},
-    {lat: -37.765015, lng: 145.133858, type: 'sport', title: 'Titulo Prueba 1', text: 'Texto de prueba 1'},
-    {lat: -37.770104, lng: 145.143299, type: 'sport', title: 'Titulo Prueba 1', text: 'Texto de prueba 1'},
-    {lat: -37.773700, lng: 145.145187, type: 'arts', title: 'Titulo Prueba 1', text: 'Texto de prueba 1'},
-    {lat: -37.774785, lng: 145.137978, type: 'bar', title: 'Titulo Prueba 1', text: 'Texto de prueba 1'},
-    {lat: -37.819616, lng: 144.968119, type: 'sport', title: 'Titulo Prueba 1', text: 'Texto de prueba 1'},
-    {lat: -38.330766, lng: 144.695692, type: 'arts', title: 'Titulo Prueba 1', text: 'Texto de prueba 1'},
-    {lat: -39.927193, lng: 175.053218, type: 'sport', title: 'Titulo Prueba 1', text: 'Texto de prueba 1'},
-    {lat: -41.330162, lng: 174.865694, type: 'arts', title: 'Titulo Prueba 1', text: 'Texto de prueba 1'},
-    {lat: -42.734358, lng: 147.439506, type: 'bar', title: 'Titulo Prueba 1', text: 'Texto de prueba 1'},
-    {lat: -42.734358, lng: 147.501315, type: 'arts', title: 'Titulo Prueba 1', text: 'Texto de prueba 1'},
-    {lat: -42.735258, lng: 147.438000, type: 'bar', title: 'Titulo Prueba 1', text: 'Texto de prueba 1'},
-    {lat: -43.999792, lng: 170.463352, type: 'arts', title: 'Titulo Prueba 1', text: 'Texto de prueba 1'}
-    ]
+    }    
 
   </script>
   <!-- cargamos .js markerclusterer -->
@@ -220,5 +116,3 @@
   <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAf3HlMS638SZdNMtrHTXzd1vUnq9XnKXQ&callback=myMap"></script>
 </body>
 </html>
-
-
